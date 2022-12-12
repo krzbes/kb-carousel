be63c38cedca9753d36cb60fcb39d3a1d44c3a35
krzbes <k2b.you@gmail.com>
2022-12-12 12:49:24 +0100

    Initialize plugin Add plugin description

---
+++ kb-carousel.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package kbCarousel
+ */
+/*
+Plugin Name: kbCarousel
+Plugin URI: https://github.com/krzbes/kb-carousel
+Description: My own implementation of <a href="https://owlcarousel2.github.io/OwlCarousel2/">Owl Carousel</a> plugin for WordPress
+Author: krzbes
+Version: 1.0.0
+ */
